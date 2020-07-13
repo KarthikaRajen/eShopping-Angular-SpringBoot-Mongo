920f3fc820ef8251a5c16f486d0ad5107bce20cf
kumarvel-sekar <k4kartvel@gmail.com>
2020-07-13 17:35:18 -0400

    Spring and Angular code changes

--- frontend/src/app/models/Order.ts
+++
@@ -1,9 +0,0 @@
-export class Order {
-    orderId: number;
-    productId: string;
-    productName: string;
-    orderDate: string;
-    deliveryDate: string;
-    amt: number;
-    categoryId: string;
-}
--- frontend/src/endpoint.ts
+++
@@ -1,9 +0,0 @@
-export const CATERGORY_URL = "/api/product/category/";
-
-export const PRODUCT_URL = "/api/product/";
-
-export const CHECKOUT_URL = `/api/checkout`;
-
-export const CART_URL = `/api/cart`;
-
-export const ORDER_URL = `/api/order`;
